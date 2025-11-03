0a30698c54199834426893b246d598a911c447a1
Krich5 <165947471+Krich5@users.noreply.github.com>
2025-11-03 09:15:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 /* Global config for the new single-password flow */
 window.APP_CONFIG = {
   BRAND: "CXAS Training Portal",
-  PASSWORD: "letmein", // <-- change to your single password
+  PASSWORD: "password",
   VERSION: "v1"
 };
 </script>
